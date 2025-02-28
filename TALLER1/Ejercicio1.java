773d909367e4d45fc368b2fd6a021623fb27ce22
1oscarflorez1 <oscarflorezramos@gmail.com>
2025-02-28 00:38:26 +0000

    Vehiculo

---
+++ TALLER1/Ejercicio1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el valor de N: ");
+        int N = scanner.nextInt();
+        int[] arreglo = new int[N];
+        int suma = 0;
+
+        for (int i = 0; i < N; i++) {
+            System.out.print("Ingrese el número " + (i + 1) + ": ");
+            arreglo[i] = scanner.nextInt();
+            suma += arreglo[i];
+        }
+
+        System.out.println("La suma de los números es: " + suma);
+    }
+}
