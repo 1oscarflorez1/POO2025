900dbd2df6d2a32811320d6486d0e2952a891804
1oscarflorez1 <oscarflorezramos@gmail.com>
2025-03-13 21:37:11 +0000

    Se crean arreglos para parqueo

---
+++ EVALUACION1/Tarifador.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Tarifador {
+    public static void main(String[] args) {
+        //Se crean arreglos para los lugares de parqueo
+        int[] bajoCilindraje = new int[20];
+        int[] altoCilindraje = new int[10];
+        System.out.println("(0cc - 399cc): " + Arrays.toString(bajoCilindraje));
+        System.out.println("(400cc - 2500cc): " + Arrays.toString(altoCilindraje));
+        System.out.println("¡Que suerte! Todos los puestos estan disponibles... por ahora.\n");
+
+
+        Scanner scanner = new Scanner(System.in);
+        
+        //BIENVENIDA
+        System.out.println("BIENVENIDOS A ParkingNow, SU PARQUEO DE CONFIANZA!");
+        //Imprimiendo valor tarifaS...
+        System.out.println("$ TARIFAS: $ ");
+        System.out.println("         0 ≤ 30 minutos = $0\n         30 < 60 minutos = $800\n         ≥ 60 minutos = $2000");
+        
+        //Se crea el menu principal con while para la iteracion.
+        //MENU PRINCIPAL
+        while (true){
+        System.out.println("_-MENU PRINCIPAL-_");
+        System.out.println("INGRESE EL NÚMERO QUE CORRESPONDA:");
+        System.out.println("- REGISTRA TU MOTO..");
+        System.out.println("1. Bajo cilindraje (0cc - 399cc)..");
+        System.out.println("2. Alto cilindraje (400cc - 2500cc)..");
+        System.out.println("3. COBROS..");
+        System.out.println("4. CERRAR APLICATIVO.");
+        int menu = scanner.nextInt();
+
+        if (menu == 1){
+            //Se registrará como bajo cilindraje
+            System.out.println("3. COBROS..");
+            int lugar = scanner.nextInt();
+            if(lugar >= 0 && lugar >20){
+                if()
+            }
+
+        }
+
+        if (menu == 2){
+
+        }
+
+        if (menu == 3){
+            System.out.println("El programa se cerrará.");
+        System.exit(0);
+        }
+
+        
+        
+        
+        // Reconociendo moto...
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Ingrese su tipo de moto: ");
+        System.out.println("1. Bajo cilindraje (0cc - 399cc)");
+        System.out.println("2. Alto cilindraje (400cc - 2500cc)");
+        int tipoMoto = lectura.nextInt();
+        
+        System.out.println("Ha seleccionado que su ");
+    
+        lectura.close();
+    }
+    }
+}
