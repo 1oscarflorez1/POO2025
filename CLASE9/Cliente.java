6bf51d48385856d53dd33928b90f66b5ac511b46
1oscarflorez1 <oscarflorezramos@gmail.com>
2025-04-10 18:20:35 +0000

    Se crean los setters y getters para las variables de la clase Producto

--- CLASE9/Cliente.java
+++ CLASE9/Cliente.java
@@ -1,12 +1,56 @@
 public class Cliente {
 
-    //atributos
+//Atributos
     private String cedula;
     private String nombre;
     private String direccion;
     private int celular;
     private String correo; 
-    
-    //metodos
+//Constructor
+    public Cliente(String cedula, String nombre, String direccion, int celular, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.celular = celular;
+        this.correo = correo;
+    }
+//Getters
+    public String getCedula() {
+        return cedula;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+//Setters
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
--- CLASE9/Producto.java
+++ CLASE9/Producto.java
@@ -1,25 +1,38 @@
 public class Producto {
         //Clase padre para todo tipo de producto
     
-    //Atributos
+//Atributos
     private int numero;
     private int precio;
     private String descripcion;
 
-    // Constructor
+// Constructor
     public Producto(int numero, int precio, String descripcion) {
         this.numero = numero;
         this.precio = precio;
         this.descripcion = descripcion;
-
-    // Getters
+    }
+// Getters
     public int getNumero() {
         return numero;
     }
-
     public int getPrecio() {
         return precio;
     }
+    public String getDescripcion() {
+        return descripcion;
+    }
+// Setters
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
     //metodos
 
-}
+
