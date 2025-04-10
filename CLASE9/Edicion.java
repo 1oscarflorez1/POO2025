23bfb40ecae593d2ccbcd07630526e9b8194cc28
1oscarflorez1 <oscarflorezramos@gmail.com>
2025-04-10 18:38:02 +0000

    Se añade atributo softwareEditor y se crea el constructor

--- CLASE9/Edicion.java
+++ CLASE9/Edicion.java
@@ -1,8 +1,13 @@
 public class Edicion extends Producto {
     //Atributos
     private double horas;
-    private String softwareUtilizado;
-
+    private String softwareEditor;
+//Constructor
+public Edicion(int numero, int precio, String descripcion, int horas, String softwareUtilizado) {
+    super(numero, precio, descripcion);
+    this.horas = horas;
+    this.softwareUtilizado = softwareUtilizado;
+}
     //metodo
     System.out.println("Servicio de edición:");
     System.out.println("Horas: " + horas);
