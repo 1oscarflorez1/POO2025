0158811248b08d6a43b886676bd794def05ef1af
1oscarflorez1 <oscarflorezramos@gmail.com>
2025-05-22 14:19:34 +0000

    Se añaden metodos que usaran los personajes

--- Momento3/Personaje.java
+++ Momento3/Personaje.java
@@ -8,4 +8,38 @@ abstract class Personaje {
     protected boolean ataqueEspecialUsado = false;
     protected final int MIN_DANO = 10;
     protected final int MAX_DANO = 30;
+
+//constructor de personaje
+ public Personaje(String nombre) {
+        this.nombre = nombre;
+    }
+      public void atacar(Personaje oponente) {
+        Random rand = new Random();
+        int dano = rand.nextInt((MAX_DANO - MIN_DANO) + 1) + MIN_DANO;
+        oponente.recibirDano(dano);
+        System.out.println(this.nombre + " ataca a " + oponente.getNombre() + " causando " + dano + " puntos de daño.");
+    }
+
+    public abstract void ataqueEspecial(Personaje oponente);
+
+    public void recibirDano(int dano) {
+        this.puntosDeVida -= dano;
+        if (this.puntosDeVida < 0) this.puntosDeVida = 0;
+    }
+
+    public boolean estaVivo() {
+        return this.puntosDeVida > 0;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getPuntosDeVida() {
+        return this.puntosDeVida;
+    }
+
+    public boolean puedeUsarEspecial() {
+        return !ataqueEspecialUsado;
+    }
 }
