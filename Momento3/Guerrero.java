e84056bfbd1a448f74ed0b6e56900c8357b7c0f1
1oscarflorez1 <oscarflorezramos@gmail.com>
2025-05-22 14:29:23 +0000

    Añado metodo heredado ataque especial

--- Momento3/Guerrero.java
+++ Momento3/Guerrero.java
@@ -4,3 +4,16 @@ public class Guerrero extends Personaje {
         super(nombre);
         this.puntosDeVida = 120;
 }
+  @Override
+    public void ataqueEspecial(Personaje oponente) {
+        if (!ataqueEspecialUsado) {
+            Random rand = new Random();
+            int dano = rand.nextInt(21) + 20; // 20 a 40
+            oponente.recibirDano(dano);
+            ataqueEspecialUsado = true;
+            System.out.println(nombre + " usa su ataque especial con espada feroz y causa " + dano + " de daño.");
+        } else {
+            System.out.println(nombre + " ya ha usado su ataque especial.");
+        }
+    }
+}
